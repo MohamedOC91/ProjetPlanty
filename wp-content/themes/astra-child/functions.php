b4b6b0b5bada75612d7548fd4f91877881707879
Mohamed Moukaddi <Mohamedmoukaddi0@gmail.com>
2023-09-25 02:50:03 +0200

    site terminé

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -23,3 +23,16 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+function ajouter_lien_admin_au_menu($items, $args) {
+    // Vérifier si l'utilisateur est connecté
+   if (is_user_logged_in() && $args->menu == 'menu-planty') {
+        // Ajouter le lien "Admin" au menu
+        $admin_link = '<li class="menu-item"><a class="adminlink" href="' . admin_url() . '">Admin</a></li>';
+        
+        // Insérer le lien à la fin des éléments du menu
+        $items .= $admin_link;
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_au_menu', 10, 2);
